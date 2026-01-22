fbf269609ebee58da64cf91383da43e0a0a4166e
OmarEhab14 <www.omarehab3710@gmail.com>
2026-01-22 19:55:53 +0200

    feat: add initial files

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#this script will be the main dbms script
+#I don't know what it handles yet but of course it handles something xD
+
+#some shared variables among all files
+db_dir="$HOME"/db_dir;
+
+
+check_if_dir_exists(){
+    [ -d $db_dir ] || mkdir -p $db_dir
+}
+
+
+#function for creating database
