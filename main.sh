87fd842797004b9699727f27128c8d1eb5e30087
AbdallahEhab <abdallahehab3710@gmail.com>
2026-01-22 20:09:41 +0200

    feat: added main select statement

--- main.sh
+++ main.sh
@@ -4,13 +4,31 @@
 #I don't know what it handles yet but of course it handles something xD
 
 #some shared variables among all files
-db_dir="$HOME"/db_dir;
+dbms_dir="$HOME"/dbms_dir;
 
 
-check_if_dir_exists(){
-    [ -d $db_dir ] || mkdir -p $db_dir
-    
+make_dbms_dir(){
+    [ -d $dbms_dir ] || mkdir -p $dbms_dir;
 }
 
-
 #function for creating database
+select option in "create database" "connect to database" "list databases" "drop database";
+    do
+        case "$REPLY" in
+        1)
+            create_db
+            ;;
+        2)
+            connect_to_db
+            ;;
+        3)
+            list_dbs
+            ;;
+        4)
+            drop_db
+            ;;
+        *)
+            "$REPLY is not a valid option"
+            ;;
+        esac
+    done
