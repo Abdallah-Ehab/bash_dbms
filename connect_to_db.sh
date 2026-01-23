ed076cdeeeed079c1b87158298b19c6bf7c2ea75
AbdallahEhab <abdallahehab3710@gmail.com>
2026-01-23 13:25:06 +0200

    refactor: refactored connect_to_db.sh and added local variables

--- connect_to_db.sh
+++ connect_to_db.sh
@@ -3,39 +3,40 @@
 
 ## id db exists connect if not ask the user if he wants to create and connect at the same time
 ## connecting is simple you basically cd to the db directory
-db_name;
-is_connected=false;
+
 connect_to_db(){
+    local db_name="";
     [ $# -eq 1 ] || read -p "enter the name of the db to connect to : " db_name;
     if [ -d "$dbms_dir/$db_name" ]; then
     echo
-    echo -n connecting
+    echo -n connecting.
+    ## just a cute loading animation nothing serious
     while "1..3"; do
     echo -n .;
     sleep 1;
     done
     echo "db connected successfully";
-    cd "$dbms_dir/$db_name";
-    is_connected=true;
+    
+    # cd "$dbms_dir/$cur_db";
+    cur_db="$db_name"
+    is_connected="true";
     else 
         echo "there is no database with this name"
-        connect_automatically
+        connect_automatically "$db_name"
     fi
 }
 
 
 connect_automatically(){
+    local db_name="$1"
     select option in "yes" "No"; do
     case $REPLY in
     1)
-       . ./create_db $db_name
-       connect_to_db $db_name
+       . ./create_db "$1"
+       connect_to_db "$1"
+       break;
        ;;
-    2)
-        echo "failed to connect to db";
-        exit;
-        ;;
-    *)
+    2|*)
         echo "failed to connect to db";
         exit;
         ;;
@@ -43,4 +44,4 @@ connect_automatically(){
     done
 }
 
-[ $# -eq 1 ] && connect_to_db $1 || connect_to_db
+connect_to_db "$@";
